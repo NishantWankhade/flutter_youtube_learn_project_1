f182622206d98a643e81b48441720a2bb1cc20a4
NGW334 <ngw334@gmail.com>
2022-12-05 06:01:35 +0530

    RangeSelectorPage

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -9,6 +9,9 @@ class RangeSelectorPage extends StatefulWidget {
 }
 
 class _RangeSelectorPageState extends State<RangeSelectorPage> {
+  int _min = 0;
+  int _max = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,21 +28,23 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const RangeSelectorTextFormField(
-                label: 'Minimum',
+              RangeSelectorTextFormField(
+                labelText: 'Minimum',
+                intValueSetter: (value) => _min = value,
               ),
               const SizedBox(
                 height: 12,
               ),
-              const RangeSelectorTextFormField(
-                label: 'Maximum',
+              RangeSelectorTextFormField(
+                labelText: 'Maximum',
+                intValueSetter: (value) => _max = value,
               ),
             ],
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.arrow_forward),
+          child: const Icon(Icons.arrow_forward),
           onPressed: () {
             // TODO: Validate the form
             // TODO: Navigate to the generator page
@@ -49,26 +54,36 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
 }
 
 class RangeSelectorTextFormField extends StatelessWidget {
-  final String label;
-
   const RangeSelectorTextFormField({
     Key? key,
-    required this.label,
+    required this.labelText,
+    required this.intValueSetter,
   }) : super(key: key);
+
+  final String labelText;
+  final void Function(int value) intValueSetter;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(8)),
         ),
-        // labelText: label,
+        labelText: labelText,
       ),
       keyboardType: const TextInputType.numberWithOptions(
         decimal: false,
         signed: true,
       ),
+      validator: (value) {
+        if (value == null || int.tryParse(value) == null) {
+          return 'Must be an integer!';
+        } else {
+          return null;
+        }
+      },
+      onSaved: (newValue) => intValueSetter(int.parse(newValue ?? '')),
     );
   }
 }
